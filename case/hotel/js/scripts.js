4e37cc2cbbb9fde544c1fa6ffdfb4d50c730060c
zeronwo <43133698+zeronwo@users.noreply.github.com>
2021-07-23 15:59:07 +0800

    up hotel

---
+++ case/hotel/js/scripts.js
@@ -0,0 +1,62 @@
+
+$(document).ready(function () {
+
+    //手機選單
+    $(".menu_rwd_toggle").click(function () {
+        $(".sidemenu").toggleClass("active");
+        $("body").toggleClass("menu-open");
+
+    });
+
+    var bodyClass = document.body.classList,
+        lastScrollY = 200;
+    window.addEventListener('scroll', function () {
+        var st = this.scrollY;
+        // 判斷是向上捲動，而且捲軸超過 200px
+        if (st < lastScrollY) {
+            bodyClass.remove('open');
+        } else {
+            bodyClass.add('open');
+        }
+        lastScrollY = st;
+    });
+
+    $(".language .menu_btn").click(function () {
+        $(".language ul").toggleClass("active");
+    });
+
+    $(".menu_button").click(function () {
+        $(this).toggleClass("active");
+    });
+    //首頁輪播圖
+    $('.top_big_slide').owlCarousel({
+        loop: true, //循環播放
+        animateOut: 'fadeOut',
+        items: 1,
+        margin: 0,
+        autoplay: true, //自動撥放
+        dots: false,
+        smartSpeed: 450
+    });
+
+    $(".datepicker").datepicker({
+    });
+
+    $('.reserv_tab li').click(function (e) {
+        var $tabIndex = $(this).index();
+        $(this).addClass('active').siblings(".reserv_tab li").removeClass('active');
+        $('.reserv_main>li').eq($tabIndex).fadeIn().siblings(".reserv_main>li").hide();
+        $('.reserv_main>li').eq($tabIndex).addClass("active");
+      });
+    
+
+
+    //lighrbox
+    $(document).on('lity:resize', function (event, instance) {
+        console.log('Lightbox resized');
+    });
+
+});
+
+
+
