3d264fcf400a7c6d74d5a07acd219c238c198790
zeronwo <zeronwo7@gmail.com>
2022-07-05 14:30:31 +0800

    0705

---
+++ case/vision/world_cup/js/scripts.js
@@ -0,0 +1,141 @@
+
+$(document).ready(function () {
+
+    //LOAD
+    $("#element").introLoader({
+      animation: {
+        name: 'doubleLoader',
+        options: {
+          exitFx: 'fadeOut',
+          ease: "easeInOutCirc",
+          style: 'fluoGreen',
+          delayBefore: 500,
+          exitTime: 300,
+          progbarTime: 700,
+          progbarDelayAfter: 400
+        }
+      }
+    });
+  
+
+  $('.news_inner').owlCarousel({
+
+    loop: true,
+    margin: 50,
+    center: true,
+    dots: false,
+    nav: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 2,
+        margin: 10,
+      },
+      600: {
+        items: 2,
+        margin: 20,
+      },
+      1000: {
+        items: 4,
+
+
+      }
+    }
+  });
+  $('.match_inner').owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: false,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
+
+
+      }
+    }
+  });
+  $('.group_inner').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: false,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+
+
+      }
+    }
+  });
+
+  $('.replay_inner').owlCarousel({
+
+    loop: true,
+    margin: 20,
+    center: true,
+    dots: false,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2,
+        margin: 10,
+      },
+      600: {
+        items: 2,
+        margin: 20,
+      },
+      1000: {
+        items: 3,
+
+
+      }
+    }
+  });
+  //倒數計時
+  $(function () {
+    $('.clock').countdown('2022/11/21 17:00:00', function (event) {
+      $(this).html(event.strftime('<dl class="item"><dd>%D</dd><dt>DAYS</dt></dl><dl class="item"><dd>%H</dd><dt>HRS</dt></dl><dl class="item"><dd>%M</dd><dt>MIN</dt></dl><dl class="item"><dd>%S</dd><dt>SEC</dt></dl>'));
+    });
+  })
+
+  //首頁水平輪播
+  $(".advantage-sec-slide").bxSlider({
+    minSlides: 2,
+    maxSlides: 6,
+    slideWidth: 450,
+    slideMargin: 0,
+    ticker: true,
+    speed: 100000,
+  });
+
+  AOS.init({
+    offset: 300,
+    duration: 900,
+    delay: 100,
+    once: true, //只執行一次動畫
+
+  });
+
+  // ----------------------------------------------------------------
+  //lighrbox
+  $(document).on('lity:resize', function (event, instance) {
+    console.log('Lightbox resized');
+  });
+
+});
+
+
+
